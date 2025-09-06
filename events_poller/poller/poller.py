e5a7320211b1ef1791f8bdf5145eb68f261cf663
Tomas Macko <tomik.macko@gmail.com>
2025-09-06 18:13:49 +0200

    calculate sleeps between requests

--- events_poller/poller/poller.py
+++ events_poller/poller/poller.py
@@ -1,5 +1,5 @@
 import asyncio
-from datetime import datetime
+from datetime import datetime, timezone
 import httpx
 
 from events_poller.logger import logger
@@ -13,6 +13,38 @@ class GitHubApiPoller:
         self._queue = queue
         self._aclient = httpx.AsyncClient()
         self._config = gh_poller_config
+
+    def _calculate_sleep(self, headers: httpx.Headers) -> int:
+        """Calculate sleep based on the response headers.
+
+        Docs: https://docs.github.com/en/rest/using-the-rest-api/best-practices-for-using-the-rest-api?apiVersion=2022-11-28#handle-rate-limit-errors-appropriately
+        """
+        retry_after = headers.get("retry-after")
+        rate_limit_remaining = headers.get("x-ratelimit-remaining")
+        rate_limit_reset = headers.get("x-ratelimit-reset")
+        logger.info(
+            "rate limiting related header values",
+            retry_after=retry_after,
+            rate_limit_remaining=rate_limit_remaining,
+            rate_limit_reset=rate_limit_reset,
+        )
+
+        if retry_after:
+            # Temporary rate limit
+            return int(retry_after)
+
+        # 0 requests remaining, wait till `x-ratelimit-reset` time
+        if not int(rate_limit_remaining):
+            if not rate_limit_reset:
+                return self._config.rate_limit_hard
+
+            rate_limit_reset_datetime = datetime.fromtimestamp(
+                int(rate_limit_reset), tz=timezone.utc
+            )
+            delta_time = datetime.now(timezone.utc) - rate_limit_reset_datetime
+            return int(delta_time.total_seconds())
+
+        return 0
 
     def _parse_response(self, response: httpx.Response) -> list[EventModel]:
         events = response.json()
@@ -30,7 +62,7 @@ class GitHubApiPoller:
             if e["type"] in EventTypeEnum
         ]
 
-    async def _fetch_data(self) -> list[EventModel]:
+    async def _fetch_data(self) -> tuple[list[EventModel], int]:
         try:
             res = await self._aclient.get(
                 self._config.url,
@@ -44,13 +76,17 @@ class GitHubApiPoller:
         logger.info(
             "Data fetched successfuly from the GitHubApi", status_code=res.status_code
         )
-        return self._parse_response(res)
+
+        data = self._parse_response(res)
+        sleep = max(self._config.rate_limit_base, self._calculate_sleep(res.headers))
+        return data, sleep
 
     async def run(self) -> None:
         try:
             while True:
-                data: list[EventModel] = await self._fetch_data()
+                data, sleep = await self._fetch_data()
                 await self._queue.put(data)
-                await asyncio.sleep(self._config.rate_limit)
+                logger.info("poller is going to sleep", sleep=sleep)
+                await asyncio.sleep(sleep)
         except Exception:
             logger.exception("poller.died")
--- events_poller/settings.py
+++ events_poller/settings.py
@@ -43,7 +43,8 @@ class GitHubApiConfig(BaseSettings):
     url: str = "https://api.github.com/events"
     headers: GitHubApiHeaders = GitHubApiHeaders()
     params: GitHubApiParams = GitHubApiParams()
-    rate_limit: int = 60
+    rate_limit_base: int = 60
+    rate_limit_hard: int = 3600
 
     model_config = SettingsConfigDict(settings_model_config, env_prefix="GH_")
 
